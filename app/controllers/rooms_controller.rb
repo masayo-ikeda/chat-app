516a7d7770cdfe2636cebaa618b05ad1ac6cbfb3
masayo-ikeda <masayon7777777@yahoo.co.jp>
2021-08-27 20:36:58 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,14 +2,18 @@ class RoomsController < ApplicationController
   def index
   end
 
-def create
-  @room = Room.new(room_params)
-  if @room.save
-    redirect_to root_path
-  else
-    render :new
+  def new
+    @room = Room.new
   end
-end
+  
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
 private
 
